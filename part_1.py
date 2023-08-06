ad84eb4c69313dd89f8f5f4b578cc19957c801fd
Yaakov Haimoff <hyaakov@aol.com>
2023-08-06 09:55:46 +0300

    grouped points

--- part_1.py
+++ part_1.py
@@ -1,21 +1,17 @@
+from collections import deque
 from typing import List, Optional, Union, Dict, Tuple
 import json
 import argparse
 from pathlib import Path
-
-from numpy import uint8
 from scipy import ndimage, signal
-
 import numpy as np
-from scipy import signal as sg
-from scipy.ndimage import maximum_filter
-from PIL import Image, ImageFilter, ImageEnhance
+from PIL import Image, ImageEnhance
 import matplotlib.pyplot as plt
 
-# if you wanna iterate over multiple files and json, the default source folder name is this.
+# if you want to iterate over multiple files and json, the default source folder name is this.
 DEFAULT_BASE_DIR: str = 'INSERT_YOUR_DIR_WITH_PNG_AND_JSON_HERE'
 
-# The label we wanna look for in the polygons json file
+# The label we want to look for in the polygons json file
 TFL_LABEL = ['traffic light']
 
 POLYGON_OBJECT = Dict[str, Union[str, List[int]]]
@@ -35,95 +31,42 @@ low_pass_kernel = np.array([[1 / 9, 1 / 9, 1 / 9],
                             [1 / 9, 1 / 9, 1 / 9]])
 
 
-def plot(data, title):
-    plot.i += 1
-    plt.subplot(2, 2, plot.i)
-    plt.imshow(data)
-    plt.gray()
-    plt.title(title)
-
-
-def filter_image(im: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+def test_find_tfl_lights(image_path: str, image_json_path: Optional[str] = None, fig_num=None):
     """
-    Apply a high pass filter on the image.
-
-    :param kernel:
-    :param im: The image itself as np.uint8, shape of (H, W, 3).
-    :return: The filtered image.
+    Run the attention code.
     """
+    # using pillow to load the image
+    image: Image = Image.open(image_path)
+    # converting the image to a numpy ndarray array
+    c_image: np.ndarray = np.array(image)
 
-    high_pass_image = np.zeros_like(im)
-    for channel in range(im.shape[2]):
-        channel_filtered_image = signal.convolve2d(im[:, :, channel], kernel, mode='same', boundary='symm')
+    objects = None
+    if image_json_path:
+        image_json = json.load(Path(image_json_path).open())
+        objects: List[POLYGON_OBJECT] = [image_object for image_object in image_json['objects']
+                                         if image_object['label'] in TFL_LABEL]
 
-        # Ensure the image is in an 8-bit range
-        channel_filtered_image = np.clip(channel_filtered_image, 0, 255).astype('uint8')
-        high_pass_image[:, :, channel] = channel_filtered_image
+    show_image_and_gt(c_image, objects, fig_num)
 
-    return high_pass_image
+    red_x, red_y, green_x, green_y = find_tfl_lights(c_image)
+    # 'ro': This specifies the format string. 'r' represents the color red, and 'o' represents circles as markers.
+    plt.imshow(c_image)
 
+    plt.plot(red_x, red_y, 'ro', markersize=4)
+    plt.plot(green_x, green_y, 'go', markersize=4)
 
-def inhance_image_without_affecting_brightess_and_constract(image: np.ndarray) -> np.ndarray:
-    enhancer = ImageEnhance.Color(Image.fromarray(image))
-    enhanced_image = enhancer.enhance(1.5)
-    enhanced_image_array = np.array(enhanced_image)
-    return enhanced_image_array
+    red_coordinates = list(zip(red_x, red_y))
+    green_coordinates = list(zip(green_x, green_y))
 
-X_COORDINATES = List[int]
-Y_COORDINATES = List[int]
-def cluster_points(x: List, y: List) -> Tuple[X_COORDINATES, RED_Y_COORDINATES]:
+    group_red = group_coordinates_by_order(red_coordinates)
+    group_green = group_coordinates_by_order(green_coordinates)
 
-
-
-
-
-    return x,y
-
-
-
+    first_point_form_every_red = [group[0] for group in group_red]
+    first_point_form_every_green = [group[0] for group in group_green]
 
 
 
-
-def calc_max_suppression(image: np.array, threshold: int = 120) -> object:
-    max_filtered = ndimage.maximum_filter(image, size=30)
-    # Create a binary mask by comparing the filtered image to the original grayscale image
-    mask = np.equal(max_filtered, image)
-    # Apply the mask to the original image to get the final result
-    img2 = np.uint8(mask) * image
-    y, x = np.where(mask & (img2 >= threshold))
-
-    # cluster_points(points, 20)
-    # x, y = cluster_points(x.tolist(), y.tolist())
-
-    return x.tolist(), y.tolist()
-
-
-def find_tfl_lights(c_image: np.ndarray,
-                    **kwargs) -> Tuple[RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
-    """
-    Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement.
-
-    :param c_image: The image itself as np.uint8, shape of (H, W, 3).
-    :param kwargs: Whatever config you want to pass in here.
-    :return: 4-tuple of x_red, y_red, x_green, y_green.
-    """
-    ### WRITE YOUR CODE HERE ###
-    ### USE HELPER FUNCTIONS ###
-
-    c_image = inhance_image_without_affecting_brightess_and_constract(c_image)
-    c_image = filter_image(c_image, low_pass_kernel)
-    c_image = filter_image(c_image, high_pass_kernel)
-    red_x, red_y = calc_max_suppression(c_image[:, :, 0])
-    green_x, green_y = calc_max_suppression(c_image[:, :, 2])
-
-    return red_x, red_y, green_x, green_y
-    # return [500, 700, 900], [500, 550, 600], [600, 800], [400, 300]
-
-
-#
-
-### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
+# GIVEN CODE TO TEST YOUR IMPLEMENTATION AND PLOT THE PICTURES
 def show_image_and_gt(c_image: np.ndarray, objects: Optional[List[POLYGON_OBJECT]], fig_num: int = None):
     # ensure a fresh canvas for plotting the image and objects.
     plt.figure(fig_num).clf()
@@ -150,28 +93,120 @@ def show_image_and_gt(c_image: np.ndarray, objects: Optional[List[POLYGON_OBJECT
     plt.show()
 
 
-def test_find_tfl_lights(image_path: str, image_json_path: Optional[str] = None, fig_num=None):
+def find_tfl_lights(c_image: np.ndarray,
+                    **kwargs) -> Tuple[RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
     """
-    Run the attention code.
+    Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement.
+
+    :param c_image: The image itself as np.uint8, shape of (H, W, 3).
+    :param kwargs: Whatever config you want to pass in here.
+    :return: 4-tuple of x_red, y_red, x_green, y_green.
     """
-    # using pillow to load the image
-    image: Image = Image.open(image_path)
-    # converting the image to a numpy ndarray array
-    c_image: np.ndarray = np.array(image)
+    ### WRITE YOUR CODE HERE ###
+    ### USE HELPER FUNCTIONS ###
 
-    objects = None
-    if image_json_path:
-        image_json = json.load(Path(image_json_path).open())
-        objects: List[POLYGON_OBJECT] = [image_object for image_object in image_json['objects']
-                                         if image_object['label'] in TFL_LABEL]
+    c_image = enhance_image_without_affecting_brightness_and_contrast(c_image)
+    c_image = filter_image(c_image, low_pass_kernel)
+    c_image = filter_image(c_image, high_pass_kernel)
+    red_x, red_y = calc_max_suppression(c_image[:, :, 0])
+    green_x, green_y = calc_max_suppression(c_image[:, :, 2])
 
-    show_image_and_gt(c_image, objects, fig_num)
+    return red_x, red_y, green_x, green_y
 
-    red_x, red_y, green_x, green_y = find_tfl_lights(c_image)
-    # 'ro': This specifies the format string. 'r' represents the color red, and 'o' represents circles as markers.
-    plt.imshow(c_image)
-    plt.plot(red_x, red_y, 'ro', markersize=4)
-    plt.plot(green_x, green_y, 'go', markersize=4)
+
+def enhance_image_without_affecting_brightness_and_contrast(image: np.ndarray) -> np.ndarray:
+    enhancer = ImageEnhance.Color(Image.fromarray(image))
+    enhanced_image = enhancer.enhance(1.5)
+    enhanced_image_array = np.array(enhanced_image)
+    return enhanced_image_array
+
+
+def filter_image(im: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """
+    Apply a high pass filter on the image.
+
+    :param kernel:
+    :param im: The image itself as np.uint8, shape of (H, W, 3).
+    :return: The filtered image.
+    """
+
+    high_pass_image = np.zeros_like(im)
+    for channel in range(im.shape[2]):
+        channel_filtered_image = signal.convolve2d(im[:, :, channel], kernel, mode='same', boundary='symm')
+
+        # Ensure the image is in an 8-bit range
+        channel_filtered_image = np.clip(channel_filtered_image, 0, 255).astype('uint8')
+        high_pass_image[:, :, channel] = channel_filtered_image
+        print("hi")
+
+    return high_pass_image
+
+
+def calc_max_suppression(image: np.array, threshold: int = 120) -> object:
+    max_filtered = ndimage.maximum_filter(image, size=30)
+    # Create a binary mask by comparing the filtered image to the original grayscale image
+    mask = np.equal(max_filtered, image)
+    # Apply the mask to the original image to get the final result
+    img2 = np.uint8(mask) * image
+    y, x = np.where(mask & (img2 >= threshold))
+
+    # cluster_points(points, 20)
+    # x, y = cluster_points(x.tolist(), y.tolist())
+
+    return x.tolist(), y.tolist()
+
+
+def group_coordinates_by_order(coordinates, tolerance=5):
+    # Create a queue to hold the coordinates to be processed
+    queue = deque(coordinates)
+
+    # Initialize the groups list
+    grouped_lists = []
+
+    while queue:
+        # Start a new group with the first coordinate in the queue
+        current_group = [queue.popleft()]
+
+        # List to store the indices of coordinates to be removed
+        indices_to_remove = []
+
+        # Process all remaining coordinates in the queue
+        for i in range(len(queue)):
+            x_diff = abs(current_group[-1][0] - queue[i][0])
+            y_diff = abs(current_group[-1][1] - queue[i][1])
+
+            if x_diff <= tolerance and y_diff <= tolerance:
+                # Add the coordinate to the current group and mark it for removal
+                current_group.append(queue[i])
+                indices_to_remove.append(i)
+
+        # Remove marked coordinates from the queue in reverse order to avoid index shifting
+        for index in reversed(indices_to_remove):
+            del queue[index]
+
+        # Add the current group to the grouped_lists
+        grouped_lists.append(current_group)
+
+    # Filter out groups with size 1
+    # grouped_lists = [group for group in grouped_lists if len(group) > 2]
+
+    return grouped_lists
+
+
+def plot_grouped_lists(grouped_lists):
+    for i, group in enumerate(grouped_lists):
+        # take the first point in the grouped lists
+        first_point = group[0]
+        print(first_point)
+        plt.plot( first_point, 'bo', markersize=4)
+
+
+def plot(data, title):
+    plot.i += 1
+    plt.subplot(2, 2, plot.i)
+    plt.imshow(data)
+    plt.gray()
+    plt.title(title)
 
 
 def main(argv=None):
